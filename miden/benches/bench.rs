f801a1a2518e1705e5a7359f30adb234a9198606
Cal <calum.moore@gmail.com>
2023-08-26 00:40:04 +0100

    Add benchmark runner

--- miden/benches/bench.rs
+++ miden/benches/bench.rs
@@ -1,50 +1,102 @@
-use miden::{Assembler, ProofOptions};
-use miden_processor::{MemAdviceProvider, StackInputs};
+extern crate miden_bench;
+
+use miden_bench::rpo::rpo;
+use std::time::{Duration, Instant};
 
 fn main() {
-    rpo(10000);
-    rpo(100000);
-    rpo(1000000);
+    let mut bench = Benchmark::new();
+
+    bench.benchmark_with("RPO", &[10000, 100000, 1000000], |b, p| {
+        let (setup, vm) = rpo(*p);
+        let last_vm_state = vm.last().unwrap().unwrap();
+        b.run(setup);
+        b.log("cycles", last_vm_state.clk as usize);
+    });
+
+    bench.output();
 }
 
-fn rpo(n_bytes: usize) {
-    // We can pack 7 bytes into each field element and hash 4 field elements at a time.
-    let hmerges = f64::floor(n_bytes as f64 / 4. / f64::floor(63. / 8.));
-    let code = format!(
-        r#"
-        begin
-            push.0.0.0.0
-            repeat.{hmerges}
-                push.1.1.1.1
-                hmerge
-            end
-        end
-    "#
-    );
+pub struct Benchmark<'a> {
+    timings: Vec<BenchmarkResult<'a>>,
+}
 
-    let assembler = Assembler::default();
-    let program = assembler.compile(&code).unwrap();
-    let vm = miden_processor::execute_iter(
-        &program,
-        StackInputs::default(),
-        MemAdviceProvider::default(),
-    );
+impl<'a> Benchmark<'a> {
+    pub fn new() -> Self {
+        Benchmark {
+            timings: Vec::new(),
+        }
+    }
 
-    let last_vm_state = vm.last().unwrap().unwrap();
+    pub fn benchmark<F: Fn(&mut BenchmarkRun)>(&mut self, name: &'a str, func: F) {
+        let mut run = BenchmarkRun::new();
+        func(&mut run);
+        self.timings.push(BenchmarkResult { name, run });
+    }
 
-    let start = std::time::Instant::now();
-    let (_stack, _proof) = miden::prove(
-        &program,
-        StackInputs::default(),
-        MemAdviceProvider::default(),
-        ProofOptions::default(),
-    )
-    .unwrap();
-    let end = std::time::Instant::now();
+    pub fn benchmark_with<F: Fn(&mut BenchmarkRun, &P) -> T, T, P>(
+        &mut self,
+        name: &'a str,
+        params: &[P],
+        func: F,
+    ) {
+        for p in params {
+            let mut run = BenchmarkRun::new();
+            func(&mut run, p);
+            self.timings.push(BenchmarkResult { name, run });
+        }
+    }
 
-    println!(
-        "RPO of {n_bytes} bytes took {:?} for {} cycles",
-        end - start,
-        last_vm_state.clk
-    );
+    pub fn output(&self) {
+        for res in &self.timings {
+            println!("{}: {:?}", res.name, res.run.time);
+            if !res.run.metrics.is_empty() {
+                println!("  Metrics:");
+                for (metric, value) in &res.run.metrics {
+                    println!("    {}: {}", metric, value);
+                }
+            }
+        }
+    }
+}
+
+impl<'a> Default for Benchmark<'a> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+pub struct BenchmarkRun<'a> {
+    pub time: Duration,
+    pub metrics: std::collections::HashMap<&'a str, usize>,
+}
+
+impl<'a> BenchmarkRun<'a> {
+    fn new() -> Self {
+        BenchmarkRun {
+            time: Duration::new(0, 0),
+            metrics: std::collections::HashMap::new(),
+        }
+    }
+
+    fn run<F: Fn()>(&mut self, func: F) {
+        let start_time = Instant::now();
+        func();
+        let elapsed_time = start_time.elapsed();
+        self.time = elapsed_time;
+    }
+
+    fn log(&mut self, metric: &'a str, value: usize) {
+        self.metrics.insert(metric, value);
+    }
+}
+
+pub struct BenchmarkResult<'a> {
+    pub name: &'a str,
+    pub run: BenchmarkRun<'a>,
+}
+
+impl<'a> BenchmarkResult<'a> {
+    pub fn new(name: &'a str, run: BenchmarkRun<'a>) -> Self {
+        BenchmarkResult { name, run }
+    }
 }
---
+++ miden/src/rpo.rs
@@ -0,0 +1,39 @@
+use miden::{Assembler, ProofOptions};
+use miden_processor::{MemAdviceProvider, StackInputs, VmStateIterator};
+
+pub fn rpo(n_bytes: usize) -> (impl Fn(), VmStateIterator) {
+    // We can pack 7 bytes into each field element and hash 4 field elements at a time.
+    let hmerges = f64::floor(n_bytes as f64 / 4. / f64::floor(63. / 8.));
+    let code = format!(
+        r#"
+        begin
+            push.0.0.0.0
+            repeat.{hmerges}
+                push.1.1.1.1
+                hmerge
+            end
+        end
+    "#
+    );
+
+    let assembler = Assembler::default();
+    let program = assembler.compile(code).unwrap();
+    let vm: miden_processor::VmStateIterator = miden_processor::execute_iter(
+        &program,
+        StackInputs::default(),
+        MemAdviceProvider::default(),
+    );
+
+    (
+        move || {
+            let (_stack, _proof) = miden::prove(
+                &program,
+                StackInputs::default(),
+                MemAdviceProvider::default(),
+                ProofOptions::default(),
+            )
+            .unwrap();
+        },
+        vm,
+    )
+}
