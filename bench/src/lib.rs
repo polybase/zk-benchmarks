432f6e485b3bd696e49106fac41e6748ee83c872
Cal <calum.moore@gmail.com>
2023-08-26 02:49:10 +0100

    Extract bench into shared crate

---
+++ bench/src/lib.rs
@@ -0,0 +1,86 @@
+use std::time::{Duration, Instant};
+
+pub struct Benchmark<'a> {
+    timings: Vec<BenchmarkResult<'a>>,
+}
+
+impl<'a> Benchmark<'a> {
+    pub fn new() -> Self {
+        Benchmark {
+            timings: Vec::new(),
+        }
+    }
+
+    pub fn benchmark<F: Fn(&mut BenchmarkRun)>(&mut self, name: &'a str, func: F) {
+        let mut run = BenchmarkRun::new();
+        func(&mut run);
+        self.timings.push(BenchmarkResult { name, run });
+    }
+
+    pub fn benchmark_with<F: Fn(&mut BenchmarkRun, &P) -> T, T, P>(
+        &mut self,
+        name: &'a str,
+        params: &[P],
+        func: F,
+    ) {
+        for p in params {
+            let mut run = BenchmarkRun::new();
+            func(&mut run, p);
+            self.timings.push(BenchmarkResult { name, run });
+        }
+    }
+
+    pub fn output(&self) {
+        for res in &self.timings {
+            println!("{}: {:?}", res.name, res.run.time);
+            if !res.run.metrics.is_empty() {
+                println!("  Metrics:");
+                for (metric, value) in &res.run.metrics {
+                    println!("    {}: {}", metric, value);
+                }
+            }
+        }
+    }
+}
+
+impl<'a> Default for Benchmark<'a> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+pub struct BenchmarkRun<'a> {
+    pub time: Duration,
+    pub metrics: std::collections::HashMap<&'a str, usize>,
+}
+
+impl<'a> BenchmarkRun<'a> {
+    fn new() -> Self {
+        BenchmarkRun {
+            time: Duration::new(0, 0),
+            metrics: std::collections::HashMap::new(),
+        }
+    }
+
+    pub fn run<F: Fn()>(&mut self, func: F) {
+        let start_time = Instant::now();
+        func();
+        let elapsed_time = start_time.elapsed();
+        self.time = elapsed_time;
+    }
+
+    pub fn log(&mut self, metric: &'a str, value: usize) {
+        self.metrics.insert(metric, value);
+    }
+}
+
+pub struct BenchmarkResult<'a> {
+    pub name: &'a str,
+    pub run: BenchmarkRun<'a>,
+}
+
+impl<'a> BenchmarkResult<'a> {
+    pub fn new(name: &'a str, run: BenchmarkRun<'a>) -> Self {
+        BenchmarkResult { name, run }
+    }
+}
