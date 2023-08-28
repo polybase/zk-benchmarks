a934ac88e451070b1fb064f878eaa9db4adc595a
Mateusz Mielewczyk <mateuszmlc@protonmail.com>
2023-08-28 14:39:56 +0200

    Benchmark sha256 in Miden

--- miden/benches/bench.rs
+++ miden/benches/bench.rs
@@ -1,10 +1,17 @@
 extern crate miden_bench;
 
 use bench::Benchmark;
-use miden_bench::rpo::rpo;
+use miden_bench::{rpo::rpo, sha::sha};
 
 fn main() {
     let mut bench = Benchmark::new("miden");
+
+    bench.benchmark_with("SHA256", &[1, 10, 100, 1000], |b, p| {
+        let (setup, vm) = sha(*p);
+        let last_vm_state = vm.last().unwrap().unwrap();
+        b.run(setup);
+        b.log("cycles", last_vm_state.clk as usize);
+    });
 
     bench.benchmark_with("RPO", &[10000, 100000, 1000000], |b, p| {
         let (setup, vm) = rpo(*p);
---
+++ miden/src/sha.rs
@@ -0,0 +1,44 @@
+use miden::{Assembler, ProofOptions};
+use miden_processor::{MemAdviceProvider, StackInputs, VmStateIterator};
+
+pub fn sha(n_bytes: usize) -> (impl Fn(), VmStateIterator) {
+    // Input: 32-bit value per element, 16 elements per hash. 8 elements will be spent on the previous hash.
+    // Output: 32-byte digest stored in the first 8 elements of the stack.
+    let sha_ops = n_bytes / 4 / 16;
+    let code = format!(
+        r#"
+        use.std::crypto::hashes::sha256
+
+        begin
+            push.1.2.3.4.5.6.7.8
+            repeat.{sha_ops}
+                push.9.10.11.12.13.14.15.16
+                exec.sha256::hash_2to1
+            end
+        end
+    "#
+    );
+
+    let assembler = Assembler::default()
+        .with_library(&miden_stdlib::StdLibrary::default())
+        .unwrap();
+    let program = assembler.compile(code).unwrap();
+    let vm: miden_processor::VmStateIterator = miden_processor::execute_iter(
+        &program,
+        StackInputs::default(),
+        MemAdviceProvider::default(),
+    );
+
+    (
+        move || {
+            let (_stack, _proof) = miden::prove(
+                &program,
+                StackInputs::default(),
+                MemAdviceProvider::default(),
+                ProofOptions::default(),
+            )
+            .unwrap();
+        },
+        vm,
+    )
+}
