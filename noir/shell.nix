f88583b313d71b0586e52ca3ad88da44f4350dae
Cal <calum.moore@gmail.com>
2023-08-29 22:30:04 +0100

    Seperate nix dependencies for mac

--- noir/shell.nix
+++ noir/shell.nix
@@ -7,16 +7,15 @@
     barretenberg = fetchFromGitHub {
         owner = "AztecProtocol";
         repo = "barretenberg";
-        rev = "fdd46f77531a6fcc9d9b24a698c56590d54d487e"; # same rev as in https://github.com/noir-lang/noir/blob/13df8d4fbc0104d3bbfdfacaf3afbb47e2eef4b7/flake.lock#L17C17-L17C57
+        rev = "fdd46f77531a6fcc9d9b24a698c56590d54d487e";
         sha256 = "sha256-w7yMeYp50KrlTn23TTKfYmLOQL4uIgw0wSX67v2tvvc=";
     };
     libbarretenberg = callPackage "${barretenberg}/barretenberg.nix" {};
+    isDarwin = pkgs.stdenv.isDarwin;
 in mkShell {
     packages = [
         libbarretenberg
-        pkgs.libiconv
         pkgs.llvmPackages.openmp
-        pkgs.darwin.apple_sdk.frameworks.Security
         pkgs.pkg-config
-    ];
+    ] ++ (if isDarwin then [ pkgs.darwin.apple_sdk.frameworks.Security,  pkgs.libiconv ] else []);
 }
