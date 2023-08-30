f34c6310e4ba748083b6f88c8f0544cbc8761ce1
Cal <calum.moore@gmail.com>
2023-08-30 10:41:13 +0100

    sha256 noir bench

--- noir/benches/bench.rs
+++ noir/benches/bench.rs
@@ -1,13 +1,35 @@
 extern crate noir;
+extern crate rand;
 
 use bench::Benchmark;
 use noir::{InputMap, InputValue, Proof};
+use rand::Rng;
 
 fn main() {
     let mut bench = Benchmark::from_env("noir");
 
     let backend = noir::backends::ConcreteBackend::default();
     let dir = std::env::current_dir().expect("current dir to exist");
+
+    bench.benchmark_with("SHA256", &[1, 10, 100, 1000], |b, p| {
+        let mut inputs = InputMap::new();
+
+        // Generate random bytes
+        let bytes = generate_random_u8_slice(*p)
+            .iter()
+            .map(|b| InputValue::Field((*b as u128).into()))
+            .collect::<Vec<_>>();
+
+        inputs.insert("x".to_string(), InputValue::Vec(bytes));
+
+        let proof = Proof::new(
+            &backend,
+            "sha256",
+            dir.join(format!("pkgs/sha256/{}", p)),
+            &inputs,
+        );
+        b.run(|| proof.prove());
+    });
 
     bench.benchmark("assert", |b| {
         let mut inputs = InputMap::new();
@@ -21,4 +43,13 @@ fn main() {
     });
 
     bench.output();
+}
+
+fn generate_random_u8_slice(len: usize) -> Vec<u8> {
+    let mut rng = rand::thread_rng();
+    let mut vec = Vec::with_capacity(len);
+    for _ in 0..len {
+        vec.push(rng.gen::<u8>());
+    }
+    vec
 }
