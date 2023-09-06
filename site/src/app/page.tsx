7bb308b2849148e9de3dfbaf8956bb84808e8c2b
Polybase CI <calum.moore@gmail.com>
2023-09-06 13:26:00 -0400

    Add next js font

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image'
 import { Center, Heading, VStack, HStack, Box } from '@chakra-ui/react'
 import logo from '@/img/logo.png'
 import { ResultsTable } from '@/components/ResultsTable'
+import { M_PLUS_Rounded_1c } from 'next/font/google'
+
+const rounded = M_PLUS_Rounded_1c({ subsets: ['latin'], weight: ["700"], fallback: ['SF Rounded'] })
 
 export default function Home() {
   return (
@@ -14,7 +17,7 @@ export default function Home() {
             <Center>
               <HStack>
                 <Image width={50} height={50} alt='zk-bench' src={logo} />
-                <Heading as='h1' className='display'>zk-bench</Heading>
+                <Heading as='h1' fontWeight={700} fontFamily={rounded.style.fontFamily}>zk-bench</Heading>
               </HStack>
             </Center>
           </Box>
