9561f35ada81a742804df971caf9ecbc350a1c17
Polybase CI <calum.moore@gmail.com>
2023-09-06 16:52:58 -0400

    Add faq

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,26 +1,28 @@
 "use client";
 
 import Image from 'next/image'
-import { Flex, Center, Heading, VStack, HStack, Box, Spacer } from '@chakra-ui/react'
+import { Flex, Center, Heading, VStack, HStack, Box, Spacer, Stack } from '@chakra-ui/react'
 import logo from '@/img/logo.png'
 import { ResultsTable } from '@/components/ResultsTable'
 import { M_PLUS_Rounded_1c } from 'next/font/google'
 import { ColorModeSwitcher } from '@/components/ColorModeSwitcher';
+import Faq from '../mkd/faq.mdx'
+
 
 const rounded = M_PLUS_Rounded_1c({ subsets: ['latin'], weight: ["700"], fallback: ['SF Rounded'] })
 
 export default function Home() {
   return (
     <main>
-      <Box>
+      <Box mb={20}>
         <Flex>
           <Spacer />
           <Box>
             <ColorModeSwitcher />
           </Box>
         </Flex>
-        <Center p={4}>
-          <VStack spacing={10}>
+        <Box p={4}>
+          <Stack spacing={10}>
             <Box p={2}>
               <Center>
                 <HStack>
@@ -29,9 +31,16 @@ export default function Home() {
                 </HStack>
               </Center>
             </Box>
-            <ResultsTable />
-          </VStack>
-        </Center>
+            <Center>
+              <Box maxW='container.md'>
+                <ResultsTable />
+              </Box>
+            </Center>
+            <Box maxW='container.md' margin='0 auto' width='100%' p={2} pt={10}>
+              <Faq />
+            </Box>
+          </Stack>
+        </Box>
       </Box>
     </main >
   )
