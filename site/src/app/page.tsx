411e926c946859681fa9bd126ee36ae5a7a28c53
Cal <calum.moore@gmail.com>
2023-09-07 09:35:20 -0400

    Powered by (#21)
    * Add powered by
    
    * Add favicon
    
    * Add github link

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import Image from 'next/image'
-import { Flex, Center, Heading, VStack, HStack, Box, Spacer, Stack } from '@chakra-ui/react'
+import { Flex, Center, Heading, VStack, HStack, Box, Spacer, Stack, Link } from '@chakra-ui/react'
 import logo from '@/img/logo.png'
 import { ResultsTable } from '@/components/ResultsTable'
 import { M_PLUS_Rounded_1c } from 'next/font/google'
 import { ColorModeSwitcher } from '@/components/ColorModeSwitcher';
 import Faq from '../mkd/faq.mdx'
+import { PoweredBy } from '@/components/PoweredBy';
 
 
 const rounded = M_PLUS_Rounded_1c({ subsets: ['latin'], weight: ["700"], fallback: ['SF Rounded'] })
@@ -17,9 +18,17 @@ export default function Home() {
       <Box mb={20}>
         <Flex>
           <Spacer />
-          <Box>
+          <HStack p={3}>
             <ColorModeSwitcher />
-          </Box>
+            <Box pr={2}>
+              <Link as='a' href='https://github.com/polybase/zk-benchmarks' target='_blank' fontWeight='600'>
+                github
+              </Link>
+            </Box>
+            <Box>
+              <PoweredBy />
+            </Box>
+          </HStack>
         </Flex>
         <Box p={4}>
           <Stack spacing={10}>
