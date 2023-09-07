3ab032f9bc25c02941c7c5f27ed76b44320537f1
Cal <calum.moore@gmail.com>
2023-09-07 11:47:43 -0400

    Custom annotations (#22)
    * Add annotations
    
    * Add extra note

--- site/src/components/ResultsTable.tsx
+++ site/src/components/ResultsTable.tsx
@@ -18,6 +18,7 @@ interface ResultTableProperty {
   desc?: string | JSX.Element;
   prop?: string;
   indent?: number;
+  annotations?: Record<string, string | JSX.Element>
   value?: (val: any) => any;
 }
 
@@ -29,10 +30,19 @@ const properties: ResultTableProperty[] = [{
   prop: 'zk',
   desc: 'The type of ZK used, for a detailed comparison, see the FAQ section below.',
 }, {
+  name: 'Unbounded Programs',
+  prop: 'unbounded',
+  desc: 'Unbounded programs allow inputs of variable size and loops where the number of iterations is not known at compile time. See the FAQ below for more detail on unbounded v. bounded programs.',
+}, {
   name: 'External Libraries',
   desc: 'Does the framework allow leveraging a languages existing library ecosystem? For example, in Rust this would be crates.io.',
   prop: 'existingLibSupport',
-  value: (val: boolean) => val ? "✅" : "❌",
+  // value: (val: boolean) => val ? "✅" : "❌",
+  annotations: {
+    miden: 'Miden does support loading library modules, but these must also be written in Miden. There is no existing library ecosystem for Miden.',
+    risc_zero: 'Risc Zero supports most crates in the Rust ecosystem. This is one of the powerful features of Risc Zero.',
+    noir: 'Noir does support loading library modules, but these must also be written in Noir. There is no existing library ecosystem for Noir.',
+  }
 }, {
   name: 'GPU',
   prop: 'gpu',
@@ -43,6 +53,16 @@ const properties: ResultTableProperty[] = [{
   desc: `A very simple assertion a != b, this can be used to test the frameworks minimum proving performance.`,
   prop: 'metrics.$machine.assert.results.0.time',
   value: (val?: Duration) => val ? `${(val.secs + val?.nanos / 1000000000).toFixed(2)}s` : null,
+  annotations: {
+    risc_zero: 'Risc Zero is significantly slower for this test, as the minumum number of cycles for all Risc Zero programs is 64k. Therefore this very small program still requires a large number of cycles.',
+  }
+}, {
+  name: 'Optimised Hashes',
+  prop: 'optimisedHash',
+  desc: `The hash provided by the framework has been specifically optmised i.e. its the fastest hashes available for that framework.`,
+  annotations: {
+    risc_zero: 'SHA-256 is the most optimised hash for Risc Zero, but SHA-256 is in general not ZK optimised.'
+  }
 }, {
   name: 'SHA-256',
   desc: `Calculating the SHA-256 hash for given input size. SHA-256 is NOT zk optimised so it's normal to see degraded performance compared to other hashes.`,
@@ -72,6 +92,9 @@ const properties: ResultTableProperty[] = [{
   indent: 4,
   prop: 'metrics.$machine.SHA256.results.3.time',
   value: (val?: Duration) => val ? `${(val.secs + val?.nanos / 1000000000).toFixed(2)}s` : null,
+  annotations: {
+    risc_zero: 'This benchmark exceeds the maxmium allowed time to run, and therefore is omitted.',
+  },
 }, {
   name: 'Fibonacci',
   // TODO: use markdown for this
@@ -180,7 +203,6 @@ export function ResultsTable() {
                                   <PopoverBody><Text overflowWrap='anywhere' fontSize='sm'>{prop.desc}</Text></PopoverBody>
                                 </PopoverContent>
                               </Portal>
-
                             </Popover>
                           </Box>
                         )}
@@ -189,9 +211,29 @@ export function ResultsTable() {
                     {
                       frameworks.map((fw: any) => {
                         let value = prop.value ? prop.value(getPathValue(fw, prop.prop, vars)) : getPathValue(fw, prop.prop, vars);
+                        const annotation = prop.annotations?.[fw.id]
                         return (
                           <Td key={fw.name}>
-                            {value}
+                            <HStack spacing={1}>
+                              <Box>
+                                {value}
+                              </Box>
+                              {annotation && (
+                                <Box>
+                                  <Popover>
+                                    <PopoverTrigger>
+                                      <IconButton opacity={0.3} variant='ghost' aria-label='info' size='xs' icon={<MdInfo />} />
+                                    </PopoverTrigger>
+                                    <Portal>
+                                      <PopoverContent>
+                                        <PopoverArrow />
+                                        <PopoverBody><Text overflowWrap='anywhere' fontSize='sm'>{annotation}</Text></PopoverBody>
+                                      </PopoverContent>
+                                    </Portal>
+                                  </Popover>
+                                </Box>
+                              )}
+                            </HStack>
                           </Td>
                         )
                       })
