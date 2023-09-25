1c83af35a529e8392f934f8a9b60c7a218c49908
Cal <calum.moore@gmail.com>
2023-09-24 23:45:00 -0400

    Fix mobile viewport (#50)

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Suspense } from 'react'
 import type { Metadata } from 'next'
+import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import { Providers } from './providers'
 import { PostHogPageview } from '@/components/PostHogPageview'
@@ -19,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Suspense>
         <PostHogPageview />
       </Suspense>
